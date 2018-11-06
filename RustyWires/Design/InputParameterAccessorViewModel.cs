8428234e683ff93458e2d50d3a8e19d6a0a5cbec
Ben Leedom <africanswallow@gmail.com>
2018-11-05 21:20:20 -0600

    Add view model for InputParameterAccessor

---
+++ RustyWires/Design/InputParameterAccessorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RustyWires.SourceModel;
+
+namespace RustyWires.Design
+{
+    public class InputParameterAccessorViewModel : BasicNodeViewModel
+    {
+        public InputParameterAccessorViewModel(InputParameterAccessor accessor, string name) 
+            : base(accessor, name)
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool CanChangeTemplate => false;
+    }
+}
