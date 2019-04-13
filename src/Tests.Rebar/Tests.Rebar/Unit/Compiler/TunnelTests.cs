3a4709a09e5a214c8df3b61580bb5a6b3dbb61c7
Ben Leedom <africanswallow@gmail.com>
2019-04-13 16:14:34 -0500

    Add test cases for wiring reference types through Tunnels.

--- src/Tests.Rebar/Tests.Rebar/Unit/Compiler/TunnelTests.cs
+++ src/Tests.Rebar/Tests.Rebar/Unit/Compiler/TunnelTests.cs
@@ -8,6 +8,7 @@ using NationalInstruments.DataTypes;
 using NationalInstruments.Dfir;
 using Rebar.Common;
 using Rebar.Compiler;
+using Rebar.Compiler.Nodes;
 
 namespace Tests.Rebar.Unit.Compiler
 {
@@ -40,6 +41,38 @@ namespace Tests.Rebar.Unit.Compiler
 
             VariableReference outputVariable = tunnel.OutputTerminals[0].GetTrueVariable();
             Assert.IsTrue(outputVariable.Type.IsInt32());
+        }
+
+        [TestMethod]
+        public void FrameInputTunnelWithReferenceTypeWiredIn_SetVariableTypes_TypeAndLifetimeSetOnOutputVariable()
+        {
+            DfirRoot dfirRoot = DfirRoot.Create();
+            Frame frame = Frame.Create(dfirRoot.BlockDiagram);
+            Tunnel tunnel = CreateInputTunnel(frame);
+            ExplicitBorrowNode borrow = ConnectExplicitBorrowToInputTerminal(tunnel.InputTerminals[0]);
+            ConnectConstantToInputTerminal(borrow.InputTerminals[0], PFTypes.Int32, false);
+
+            RunSemanticAnalysisUpToSetVariableTypes(dfirRoot);
+
+            VariableReference outputVariable = tunnel.OutputTerminals[0].GetTrueVariable();
+            Assert.IsTrue(outputVariable.Type.IsImmutableReferenceType());
+            Assert.IsTrue(outputVariable.Lifetime.DoesOutlastDiagram);
+        }
+
+        [TestMethod]
+        public void FrameOutputTunnelWithReferenceTypeWiredIn_SetVariableTypes_TypeAndEmptyLifetimeSetOnOutputVariable()
+        {
+            DfirRoot dfirRoot = DfirRoot.Create();
+            Frame frame = Frame.Create(dfirRoot.BlockDiagram);
+            Tunnel tunnel = CreateOutputTunnel(frame);
+            ExplicitBorrowNode borrow = ConnectExplicitBorrowToInputTerminal(tunnel.InputTerminals[0]);
+            ConnectConstantToInputTerminal(borrow.InputTerminals[0], PFTypes.Int32, false);
+
+            RunSemanticAnalysisUpToSetVariableTypes(dfirRoot);
+
+            VariableReference outputVariable = tunnel.OutputTerminals[0].GetTrueVariable();
+            Assert.IsTrue(outputVariable.Type.IsImmutableReferenceType());
+            Assert.IsTrue(outputVariable.Lifetime.IsEmpty);
         }
 
         private Tunnel CreateInputTunnel(Frame frame)
